bazelbuild/rules_android

commit 695bee529d66c670a98eb13e17966f4f5e2e3ac7

Remove min SDK Validation action.
There's no point in validating the value since we force bump it.

PiperOrigin-RevId: 606300560
Change-Id: I07307cce1761f6c0c1b00938ddeeaaa41137e23c

src/tools/enforce_min_sdk_floor/enforce_min_sdk_floor.py
@@ -27,7 +27,6 @@ from absl import flags
 import lxml.etree as ET
 
 BUMP = "bump"
-VALIDATE = "validate"
 SET_DEFAULT = "set_default"
 
 USES_SDK = "uses-sdk"
@@ -38,8 +37,8 @@ FLAGS = flags.FLAGS
 flags.DEFINE_enum(
     "action",
     None,
-    [BUMP, VALIDATE, SET_DEFAULT],
-    f"Action to perform, either {BUMP}, {VALIDATE}, or {SET_DEFAULT}")
+    [BUMP, SET_DEFAULT],
+    f"Action to perform: either {BUMP} or {SET_DEFAULT}")
 flags.DEFINE_string(
     "manifest",
     None,
@@ -164,8 +163,6 @@ def _BumpMinSdk(xml_content, min_sdk_floor):
       f"minSdkVersion attribute specified in the manifest ({min_sdk}) "
       + f"is not less than the floor ({min_sdk_floor}). Manifest unchanged.")
 
-
-def _ValidateMinSdk(xml_content, min_sdk_floor):
   """Checks the min SDK in xml_content and raises MinSdkError if it is either not specified or less than the floor.
 
   Args:
@@ -274,14 +271,8 @@ def main(unused_argv):
     )
     with open(output_path, "wb") as f:
       f.write(out_contents)
-
-  elif FLAGS.action == VALIDATE:
-    try:
-      log_message = _ValidateMinSdk(manifest, FLAGS.min_sdk_floor)
-    except MinSdkError as e:
-      sys.exit(str(e))
   else:
-    sys.exit(f"Action must be either {BUMP} or {VALIDATE}")
+    sys.exit(f"Action must be either {BUMP} or {SET_DEFAULT}")
 
   if FLAGS.log is not None:
     log_path = FLAGS.log

rules/android_binary_internal/impl.bzl
@@ -109,14 +109,6 @@ def _process_resources(ctx, manifest_ctx, java_package, **unused_ctxs):
 def _validate_manifest(ctx, packaged_resources_ctx, **unused_ctxs):
     validation_outputs = []
 
-    min_sdk_validation_output = _resources.validate_min_sdk(
-        ctx,
-        manifest = packaged_resources_ctx.processed_manifest,
-        enforce_min_sdk_floor_tool = get_android_toolchain(ctx).enforce_min_sdk_floor_tool.files_to_run,
-    )
-    if min_sdk_validation_output:
-        validation_outputs.append(min_sdk_validation_output)
-
     if acls.in_android_binary_starlark_rollout(str(ctx.label)):
         manifest_validation_output = _resources.validate_manifest(
             ctx,

rules/resources.bzl
@@ -1235,49 +1235,6 @@ def _set_default_min_sdk(
 
     return _ManifestContextInfo(**manifest_ctx)
 
-def _validate_min_sdk(
-        ctx,
-        manifest,
-        floor = _min_sdk_version.DEPOT_FLOOR,
-        enforce_min_sdk_floor_tool = None):
-    """Validates that the min SDK attribute of AndroidManifest is at least at the floor.
-
-    Args:
-      ctx: The rules context.
-      manifest: File. The AndroidManifest.xml file.
-      floor: int. The min SDK floor. No validation is done if floor <= 0.
-      enforce_min_sdk_floor_tool: FilesToRunProvider. The enforce_min_sdk_tool executable or
-        FilesToRunprovider
-
-    Returns:
-      A file containing the log of validation results, or None if validation not performed.
-    """
-    if not manifest or floor <= 0:
-        return None
-
-    args = ctx.actions.args()
-    args.add("-action", "validate")
-    args.add("-manifest", manifest)
-    args.add("-min_sdk_floor", floor)
-
-    out_dir = "_migrated/_min_sdk_validated/" + ctx.label.name + "/"
-    log = ctx.actions.declare_file(
-        out_dir + "log.txt",
-    )
-    args.add("-log", log.path)
-
-    ctx.actions.run(
-        executable = enforce_min_sdk_floor_tool,
-        inputs = [manifest],
-        outputs = [log],
-        arguments = [args],
-        mnemonic = "ValidateMinSdkFloor",
-        progress_message = "Validating AndroidManifest min SDK %s" % str(ctx.label),
-        toolchain = None,
-    )
-
-    return log
-
 def _validate_manifest(
         ctx,
         manifest,
@@ -2169,7 +2126,6 @@ resources = struct(
 
     # Exposed for android_binary
     is_resource_shrinking_enabled = _is_resource_shrinking_enabled,
-    validate_min_sdk = _validate_min_sdk,
     validate_manifest = _validate_manifest,
     shrink = _shrink,
     optimize = _optimize,

src/tools/enforce_min_sdk_floor/enforce_min_sdk_floor_test.py
@@ -18,7 +18,6 @@ import xml.etree.ElementTree as ET
 
 from src.tools.enforce_min_sdk_floor.enforce_min_sdk_floor import _BumpMinSdk
 from src.tools.enforce_min_sdk_floor.enforce_min_sdk_floor import _SetDefaultMinSdk
-from src.tools.enforce_min_sdk_floor.enforce_min_sdk_floor import _ValidateMinSdk
 
 from src.tools.enforce_min_sdk_floor.enforce_min_sdk_floor import MIN_SDK_ATTRIB
 from src.tools.enforce_min_sdk_floor.enforce_min_sdk_floor import MinSdkError
@@ -110,32 +109,6 @@ class EnforceMinSdkFloorTest(unittest.TestCase):
     out, _ = _SetDefaultMinSdk(MANIFEST_MIN_SDK, "14")
     self.assertEqual(out, MANIFEST_MIN_SDK)
 
-  def test_validate_no_min_sdk_floor(self):
-    _ = _ValidateMinSdk(MANIFEST_NO_USES_SDK, 0)
-
-  def test_validate_no_uses_sdk(self):
-    self.assertRaises(MinSdkError,
-                      _ValidateMinSdk,
-                      xml_content=MANIFEST_NO_USES_SDK,
-                      min_sdk_floor=5)
-
-  def test_validate_no_min_sdk_attrib(self):
-    self.assertRaises(MinSdkError,
-                      _ValidateMinSdk,
-                      xml_content=MANIFEST_NO_MIN_SDK,
-                      min_sdk_floor=19)
-
-  def test_validate_min_sdk_attrib_placeholder(self):
-    _ = _ValidateMinSdk(MANIFEST_MIN_SDK_PLACEHOLDER, 21)
-
-  def test_validate_higher_min_sdk(self):
-    _ = _ValidateMinSdk(MANIFEST_MIN_SDK, 8)
-
-  def test_validate_lower_min_sdk(self):
-    self.assertRaises(MinSdkError,
-                      _ValidateMinSdk,
-                      xml_content=MANIFEST_MIN_SDK,
-                      min_sdk_floor=18)
 
 if __name__ == "__main__":
   unittest.main()

commit 413597045aaf7f4353c3060fd30e2eebfdd09c4d

Remove unreachable code caused by 695bee5
PiperOrigin-RevId: 609142945
Change-Id: I8eaac04edf44597521d8f5f5ad93be11e928ea6e

src/tools/enforce_min_sdk_floor/enforce_min_sdk_floor.py
@@ -163,44 +163,6 @@ def _BumpMinSdk(xml_content, min_sdk_floor):
       f"minSdkVersion attribute specified in the manifest ({min_sdk}) "
       + f"is not less than the floor ({min_sdk_floor}). Manifest unchanged.")
 
-  """Checks the min SDK in xml_content and raises MinSdkError if it is either not specified or less than the floor.
-
-  Args:
-    xml_content: str, the contents of the AndroidManifest.xml file
-    min_sdk_floor: int, the min SDK floor
-  Returns:
-    str: log message
-  Raises:
-    MinSdkError: The min SDK is less than the specified floor.
-  """
-  if min_sdk_floor == 0:
-    return "No min SDK floor specified."
-
-  root = ET.fromstring(xml_content)
-
-  uses_sdk = root.find(USES_SDK)
-  if uses_sdk is None:
-    raise MinSdkError(
-        "No uses-sdk element found in manifest "
-        + f"while floor is specified ({min_sdk_floor}).")
-
-  min_sdk = uses_sdk.get(MIN_SDK_ATTRIB)
-  if min_sdk is None:
-    raise MinSdkError(
-        "No minSdkVersion attribute found in manifest "
-        + f"while floor is specified ({min_sdk_floor}).")
-
-  try:
-    min_sdk_int = int(min_sdk)
-  except ValueError:
-    return f"Placeholder minSdkVersion = {min_sdk}\n min SDK floor = {min_sdk_floor}"
-
-  if min_sdk_int < min_sdk_floor:
-    raise MinSdkError(
-        f"minSdkVersion attribute specified in  the manifest ({min_sdk}) "
-        + f"is less than the floor ({min_sdk_floor}).")
-  return f"minSdkVersion = {min_sdk}\n min SDK floor = {min_sdk_floor}"
-
 
 def _SetDefaultMinSdk(xml_content, default_min_sdk):
   """Checks the min SDK in xml_content and replaces with default_min_sdk if it is not already set.