bdf9788ca67d6428217e03fbc3b5fda09e0d0cc0
Balasubramanyam Evani <bala.evani@applied.co>
2025-05-09 16:40:26 -0700

    Merge #370 by ted-xie: All android_ide_common android_tools.jar refs refactored
    Closes #370
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/bazelbuild/rules_android/pull/370 from ted-xie:deps2 97e487935e3d637725afdd8d4e2a382da769ee8a
    PiperOrigin-RevId: 756949940
    Change-Id: I72ff0769bafe187d7eb9c393b97d100d83647142

---
+++ src/tools/java/com/google/devtools/build/android/aapt2/ResourceUsageAnalyzer.java
@@ -0,0 +1,40 @@
+// Copyright 2025 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.android.build.gradle.tasks;
+
+import com.android.ide.common.resources.usage.ResourceUsageModel;
+import com.android.ide.common.resources.usage.ResourceUsageModel.Resource;
+import java.util.List;
+
+/**
+ * A stub version of the ResourceUsageAnalyzer class from AOSP.
+ *
+ * Note: The real version of this class was deleted from AOSP in 2023.
+ * This class is essentially unused in rules_android, since R8 now handles
+ * resource shrinking. This is just a workaround to avoid compilation errors
+ * to cut dependencies more expediently.
+ */
+public class ResourceUsageAnalyzer {
+    public ResourceUsageAnalyzer(Object... args) {}
+    public void shrink(Object o) {}
+    public ResourceShrinkerUsageModel model() {
+        return null;
+    }
+    public void recordClassUsages(Object o) {}
+    public void keepPossiblyReferencedResources() {}
+    public void addDeclaredResource(Object... args) {}
+    public void getResource(int i) {}
+    protected class ResourceShrinkerUsageModel extends ResourceUsageModel {
+    }
+}
