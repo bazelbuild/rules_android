6398e94ad2898adcef9e112fac634323e123c23f
Michael Hoisie <hoisie@google.com>
2026-06-11 23:44:11 -0700

    Add profiler record for `aapt2 convert`
    This is to ensure that the timing for
    `convert` is recorded in the Aapt2ResourcePackagingAction stats.
    
    PiperOrigin-RevId: 930963805
    Change-Id: I4dbb29505aec3758c8007c37f01b32b9113ac6c4

--- src/tools/java/com/google/devtools/build/android/aapt2/ResourceLinker.java
+++ src/tools/java/com/google/devtools/build/android/aapt2/ResourceLinker.java
@@ -736,6 +736,7 @@ public class ResourceLinker {
   /** Convert a proto apk to binary. */
   public Path convertProtoApkToBinary(ProtoApk protoApk) {
     try {
+      profiler.startTask("convert");
       final Path protoApkPath = protoApk.asApkPath();
       final Path working =
           workingDirectory
@@ -753,6 +754,7 @@ public class ResourceLinker {
               .add("-o", apk.toString())
               .add(protoApk.asApkPath().toString())
               .execute(String.format("Converting %s", protoApkPath)));
+      profiler.recordEndOf("convert");
       return copyAndFixCompression(apk, working);
     } catch (IOException e) {
       throw new LinkError(e);
