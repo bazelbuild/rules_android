221fe3e7472330842b840d4b94d01264d9802ea0
A Googler <no-reply@google.com>
2026-06-21 18:13:21 -0700

    Optimize DexFileSplitter by improving memory locality.
    PiperOrigin-RevId: 935770875
    Change-Id: Ie519b7ecd7a9006483f1006d3ff94e7be78d2f8e

--- src/tools/java/com/google/devtools/build/android/dexer/DexLimitTracker.java
+++ src/tools/java/com/google/devtools/build/android/dexer/DexLimitTracker.java
@@ -18,9 +18,9 @@ import com.android.dex.FieldId;
 import com.android.dex.MethodId;
 import com.android.dex.ProtoId;
 import com.android.dex.TypeList;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Interner;
 import com.google.common.collect.Interners;
+import java.util.Collections;
 import java.util.HashSet;
 
 /**
@@ -60,43 +60,46 @@ class DexLimitTracker {
   }
 
   public void track(Dex dexFile) {
-    track(new DexTrackerInfo(dexFile));
+    track(DexTrackerInfo.create(dexFile));
   }
 
   public void track(DexTrackerInfo info) {
-    fieldsSeen.addAll(info.fields);
-    methodsSeen.addAll(info.methods);
-    typesSeen.addAll(info.types);
+    Collections.addAll(fieldsSeen, info.fields);
+    Collections.addAll(methodsSeen, info.methods);
+    Collections.addAll(typesSeen, info.types);
   }
 
-  static class DexTrackerInfo {
-    final ImmutableList<String> fields;
-    final ImmutableList<String> methods;
-    final ImmutableList<String> types;
+  static final class DexTrackerInfo {
+    final String[] fields;
+    final String[] methods;
+    final String[] types;
 
-    DexTrackerInfo(Dex dexFile) {
-      int fieldCount = dexFile.fieldIds().size();
-      ImmutableList.Builder<String> fieldsBuilder =
-          ImmutableList.builderWithExpectedSize(fieldCount);
-      for (int fieldIndex = 0; fieldIndex < fieldCount; ++fieldIndex) {
-        fieldsBuilder.add(fieldSignature(dexFile, fieldIndex));
+    DexTrackerInfo(String[] fields, String[] methods, String[] types) {
+      this.fields = fields;
+      this.methods = methods;
+      this.types = types;
+    }
+
+    static DexTrackerInfo create(Dex dexFile) {
+      int typeCount = dexFile.typeIds().size();
+      String[] types = new String[typeCount];
+      for (int typeIndex = 0; typeIndex < typeCount; ++typeIndex) {
+        types[typeIndex] = typeName(dexFile, typeIndex);
       }
-      fields = fieldsBuilder.build();
+
+      int fieldCount = dexFile.fieldIds().size();
+      String[] fields = new String[fieldCount];
+      for (int fieldIndex = 0; fieldIndex < fieldCount; ++fieldIndex) {
+        fields[fieldIndex] = fieldSignature(dexFile, fieldIndex, types);
+      }
 
       int methodCount = dexFile.methodIds().size();
-      ImmutableList.Builder<String> methodsBuilder =
-          ImmutableList.builderWithExpectedSize(methodCount);
+      String[] methods = new String[methodCount];
       for (int methodIndex = 0; methodIndex < methodCount; ++methodIndex) {
-        methodsBuilder.add(methodSignature(dexFile, methodIndex));
+        methods[methodIndex] = methodSignature(dexFile, methodIndex, types);
       }
-      methods = methodsBuilder.build();
 
-      int typeCount = dexFile.typeIds().size();
-      ImmutableList.Builder<String> typesBuilder = ImmutableList.builderWithExpectedSize(typeCount);
-      for (int typeIndex = 0; typeIndex < typeCount; ++typeIndex) {
-        typesBuilder.add(typeName(dexFile, typeIndex));
-      }
-      types = typesBuilder.build();
+      return new DexTrackerInfo(fields, methods, types);
     }
   }
 
@@ -104,24 +107,24 @@ class DexLimitTracker {
     return threadLocalInterner.get().intern(dex.typeNames().get(typeIndex));
   }
 
-  private static String fieldSignature(Dex dex, int fieldIndex) {
+  private static String fieldSignature(Dex dex, int fieldIndex, String[] typeCache) {
     FieldId field = dex.fieldIds().get(fieldIndex);
     String name = dex.strings().get(field.getNameIndex());
-    String declaringClass = typeName(dex, field.getDeclaringClassIndex());
-    String type = typeName(dex, field.getTypeIndex());
+    String declaringClass = typeCache[field.getDeclaringClassIndex()];
+    String type = typeCache[field.getTypeIndex()];
     return threadLocalInterner.get().intern(declaringClass + "." + name + ":" + type);
   }
 
-  private static String methodSignature(Dex dex, int methodIndex) {
+  private static String methodSignature(Dex dex, int methodIndex, String[] typeCache) {
     MethodId method = dex.methodIds().get(methodIndex);
     ProtoId proto = dex.protoIds().get(method.getProtoIndex());
     String name = dex.strings().get(method.getNameIndex());
-    String declaringClass = typeName(dex, method.getDeclaringClassIndex());
-    String returnType = typeName(dex, proto.getReturnTypeIndex());
+    String declaringClass = typeCache[method.getDeclaringClassIndex()];
+    String returnType = typeCache[proto.getReturnTypeIndex()];
     TypeList parameterTypeIndices = dex.readTypeList(proto.getParametersOffset());
     StringBuilder parameterTypes = new StringBuilder();
     for (short parameterTypeIndex : parameterTypeIndices.getTypes()) {
-      parameterTypes.append(typeName(dex, parameterTypeIndex & 0xFFFF));
+      parameterTypes.append(typeCache[parameterTypeIndex & 0xFFFF]);
     }
     return threadLocalInterner
         .get()
